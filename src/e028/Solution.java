b2acaae59aa41f92c575ab622fcb4354e183a0cb
ColeTrammer <cole.trammer@gmail.com>
2017-05-13 12:20:24 -0700

    problem 28

--- src/e028/Solution.java
+++ src/e028/Solution.java
@@ -2,6 +2,16 @@ package e028;
 
 public class Solution {
 
-	
-	
+	public static void main(String[] args) {
+		
+		int sum = 1;
+		int n = 1;
+		for (int i = 2; i <= 1000; i += 2) {
+			for (int j = 0; j < 4; j++) {
+				n += i;
+				sum += n;
+			}
+		}
+		System.out.println(sum);
+	}
 }
